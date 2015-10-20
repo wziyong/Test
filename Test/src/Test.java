89a38d325aa31ce1ff3d7a535b46521b88ea87f8
wziyong <wziyong@126.com>
2015-10-20 10:44:21 +0800

    理解多线程

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,11 +1,10 @@
+import java.util.concurrent.SynchronousQueue;
 
 public class Test
 {
 
 	public static void main(String[] args)
 	{
-		String xx = " cat_id = 286 or  cat_id = 286 or cat_id = 286 or cat_id = 285 or";
-		
-		System.out.println(xx.substring(0, xx.lastIndexOf("or")));
+		System.out.println("aaaa".intern());
 	}
 }
